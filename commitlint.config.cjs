31e702848f0b1dd35fd5b4affc220021c6cd5a65
Theo Sementa <theosementa@gmail.com>
2025-03-26 06:41:59 +0100

    feat: update husky config

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,4 +1,4 @@
-{
+module.exports = {
   "extends": ["@commitlint/config-conventional"],
   "rules": {
       "type-enum": [2, "always", ["ci", "chore", "docs", "ticket","feat", "fix", "perf", "refactor", "revert", "style"]]
