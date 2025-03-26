335688d1af1cc0066f94ec66dd44cfe6e279f13e
Theo Sementa <theosementa@gmail.com>
2025-03-26 13:39:54 +0100

    feat: try duplication sonar

--- src/complex-method.ts
+++ src/complex-method.ts
@@ -1,46 +1,5 @@
 export class ComplexMethod {
 
-  logStatus(status: string) {
-    console.log(status);
-  }
-
-  doSomething() {
-    console.log("Doing something");
-  }
-
- checkNumber(number: number) {
-    if (number === 0) {
-      console.log("Processing 0");
-  this.processNumberA() 
-    }
-    if (number === 1) {
-      console.log("Processing 1");
-  this.processNumberA() 
-    }
-    if (number === 2) {
-      console.log("Processing 2");
- this.processNumberA() 
-    }
-  }
-  
-   processNumberA() {
-    console.log("Processing number A");
-    this.doSomething();
-    this.logStatus("Done processing A");
-  }
-  
- processNumberB() {
-    console.log("Processing number A"); // Intentional duplicate
-    this.doSomething();
-    this.logStatus("Done processing A");
-  }
-  
- processNumberC() {
-    console.log("Processing number A"); // Intentional duplicate
-    this.doSomething();
-    this.logStatus("Done processing A");
-  }
-
   static convertIntToEnglish(num: number): string {
     if (num === 0) {
       return 'zero';
@@ -68,5 +27,5 @@ export class ComplexMethod {
 
     return 'unknown';
   }
-
+  
 }
